a845fcec00629cc089492106f1a57b82202507fb
shawnroche <60703310+shawnroche@users.noreply.github.com>
2021-08-07 20:14:08 +0100

    3rd walkthrough complete LoginActivity + RegistrationActivity 07/08/2021 20:13

--- app/src/main/java/com/example/gettingbackintiit/RegistrationActivity.java
+++ app/src/main/java/com/example/gettingbackintiit/RegistrationActivity.java
@@ -8,26 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class RegistrationActivity extends AppCompatActivity {
     private EditText email, password;
-    private Button loginBTN;
-    private TextView loginQn;
+    private Button registerBTN;
+    private TextView registerQn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        loginBTN = findViewById(R.id.loginBTN);
-        loginQn = findViewById(R.id.loginQn);
+        registerBTN = findViewById(R.id.loginBTN);
+        registerQn = findViewById(R.id.loginQn);
 
-        loginQn.setOnClickListener(new View.OnClickListener() {
+        registerQn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
