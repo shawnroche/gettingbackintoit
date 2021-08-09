f8fd297e5d41856e86c54f875c0eb468e568b430
shawnroche <60703310+shawnroche@users.noreply.github.com>
2021-08-09 11:05:14 +0100

    json file added and added to firebase

--- app/src/main/java/com/example/gettingbackintiit/LoginActivity.java
+++ app/src/main/java/com/example/gettingbackintiit/LoginActivity.java
@@ -1,18 +1,26 @@
 package com.example.gettingbackintiit;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email, password;
     private Button loginBTN;
     private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +32,32 @@ public class LoginActivity extends AppCompatActivity {
         loginBTN = findViewById(R.id.loginBTN);
         loginQn = findViewById(R.id.loginQn);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = new ProgressBar(this);
+
         loginQn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if (TextUtils.isEmpty(emailString)){
+                    email.setError("Email is required");
+                }
+                if (TextUtils.isEmpty(passwordString)){
+                    email.setError("Password is required");
+                }
+                else {
+                    //progressBar.setMessage("Login in Progress")
+                }
             }
         });
     }
--- app/src/main/java/com/example/gettingbackintiit/SplashScreenActivity.java
+++ app/src/main/java/com/example/gettingbackintiit/SplashScreenActivity.java
@@ -2,7 +2,7 @@ package com.example.gettingbackintiit;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.animation.Animation;
